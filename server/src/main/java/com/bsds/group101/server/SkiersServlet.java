853261906a23873d3fabcd421c2170d5e8abfd61
0zz10_mac <zhou.zhenj@northeastern.edu>
2022-02-14 11:38:07 -0800

    create 3 API endpoints template

---
+++ server/src/main/java/com/bsds/group101/server/SkiersServlet.java
@@ -0,0 +1,20 @@
+package com.bsds.group101.server;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "SkiersServlet", value = "/SkiersServlet")
+public class SkiersServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+  }
+}
