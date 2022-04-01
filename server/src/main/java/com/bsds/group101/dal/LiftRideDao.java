f3631db0f00cb2205a8e28946dd43aa5fb60fbc2
0zz10_mac <zhou.zhenj@northeastern.edu>
2022-03-31 22:44:24 -0700

    implement getSeasonsListAtSkier method

--- server/src/main/java/com/bsds/group101/dal/LiftRideDao.java
+++ server/src/main/java/com/bsds/group101/dal/LiftRideDao.java
@@ -1,11 +1,13 @@
 package com.bsds.group101.dal;
 
-import com.bsds.group101.model.LiftRide;
 import com.zaxxer.hikari.HikariDataSource;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class LiftRideDao {
   private static HikariDataSource hikariDataSource;
@@ -15,25 +17,30 @@ public class LiftRideDao {
   }
 
   // Make sure database and table exists before you call this data insertion
-  public void createLiftRide(LiftRide newLiftRide) {
+
+  // get the total vertical for the skier for the specified ski day
+  public int getDayVerticalOfSkiersForDay(int resortId, int seasonId, int dayId, int skierId) {
     Connection conn = null;
     PreparedStatement preparedStatement = null;
-    String insertQueryStatement =
-        "INSERT INTO LiftRides (skierId, resortId, seasonId, dayId, time, waitTime, liftID) "
-            + "VALUES (?,?,?,?,?,?,?)";
+    String selectQueryStatement =
+        "SELECT SUM(liftId * 10) AS dayVertical FROM Consumer.LiftRides "
+            + "WHERE resortId = ? AND seasonId = ? AND dayId = ? AND skierId = ?;";
+
+    int dayVertical = 0;
     try {
       conn = hikariDataSource.getConnection();
-      preparedStatement = conn.prepareStatement(insertQueryStatement);
-      preparedStatement.setInt(1, newLiftRide.getSkierId());
-      preparedStatement.setInt(2, newLiftRide.getResortId());
-      preparedStatement.setInt(3, newLiftRide.getSeasonId());
-      preparedStatement.setInt(4, newLiftRide.getDayId());
-      preparedStatement.setInt(5, newLiftRide.getTime());
-      preparedStatement.setInt(6, newLiftRide.getWaitTime());
-      preparedStatement.setInt(7, newLiftRide.getLiftID());
+      preparedStatement = conn.prepareStatement(selectQueryStatement);
+      preparedStatement.setInt(1, resortId);
+      preparedStatement.setInt(2, seasonId);
+      preparedStatement.setInt(3, dayId);
+      preparedStatement.setInt(4, skierId);
 
-      // execute insert SQL statement
-      preparedStatement.executeUpdate();
+      // execute select SQL statement
+      ResultSet resultSet = preparedStatement.executeQuery();
+      while (resultSet.next()) {
+        dayVertical = resultSet.getInt("dayVertical");
+      }
+      return dayVertical;
 
     } catch (SQLException e) {
       e.printStackTrace();
@@ -49,5 +56,43 @@ public class LiftRideDao {
         se.printStackTrace();
       }
     }
+    return dayVertical;
+  }
+
+  // get the total vertical for the skier the specified resort. If no season is specified, return
+  // all seasons
+  public Map<Integer, Integer> getSeasonsListAtSkier(int skierId) {
+    Connection conn = null;
+    PreparedStatement preparedStatement = null;
+    String selectQueryStatement =
+        "SELECT seasonId, SUM(liftId * 10) AS totalVert FROM Consumer.LiftRides WHERE resortId = 56 AND dayId = 56 AND skierId = ? GROUP BY seasonId ORDER BY seasonId ASC;";
+    // instantiate a map for {"seasonID": "totalVert"}
+    Map<Integer, Integer> seasonsToVerticalMap = new ConcurrentHashMap<>();
+    try {
+      conn = hikariDataSource.getConnection();
+      preparedStatement = conn.prepareStatement(selectQueryStatement);
+      preparedStatement.setInt(1, skierId);
+      // execute select SQL statement
+      ResultSet resultSet = preparedStatement.executeQuery();
+      // iterate through the ResultSet
+      while (resultSet.next()) {
+        seasonsToVerticalMap.put(resultSet.getInt("seasonId"), resultSet.getInt("totalVert"));
+      }
+      return seasonsToVerticalMap;
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        if (conn != null) {
+          conn.close();
+        }
+        if (preparedStatement != null) {
+          preparedStatement.close();
+        }
+      } catch (SQLException se) {
+        se.printStackTrace();
+      }
+    }
+    return seasonsToVerticalMap;
   }
 }
